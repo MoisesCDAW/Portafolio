7f9cd84a0fd55eae6a451d6c25744ea0e65fca72
Moises <moisescamposdaw@gmail.com>
2024-11-08 19:50:58 +0000

    Inicio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const misDatos = {
+    datosPerso: ["Moisés Alejandro", "Campos Perdomo", "moisescamposdaw@gmail.com", "603-123456"],
+    estudios: ["Bachillerato", "FP Desarrollo de Aplicaciones Web"],
+    experiencia: [],
+    proyectos: [
+        {
+            codigo: 1,
+            url: "...",
+            titulo: "...",
+            img: "...",
+            descripcion: "..."
+        }
+    ]
+};
