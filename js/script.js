6d5fd348a242f27deccf5f001b5ef344385d92b7
Moises <moisescamposdaw@gmail.com>
2024-11-12 17:55:49 +0000

    Contenido

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 const misDatos = {
     datosPerso: [
-        "Moisés Alejandro Campos Perdomo", 
-        "07. 10. 2003", 
-        "Desarrollador de Aplicaciones Web",
-        "moisescamposdaw@gmail.com", 
-        "(+34) 603 123 456", 
-        "Canarias, España",
         {
-            github: "github.com/MoisesCDAW",
-            linkedIn: "linkedin.com/in/moisescap/",
-            instagram: "instagram.com/moisescamposp/"
+            sobreMi: "¡Hey! Soy Moisés, Desarrollador de Aplicaciones Web",
+            descripcion: `Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Perspiciatis minima aliquam impedit. Sapiente dolorum rem velit id ipsa architecto veniam.
+            Itaque magnam nulla molestias error totam, quidem minus odio culpa? `,
+            nombre: "Moisés Alejandro Campos Perdomo", 
+            nacimiento: "07. 10. 2003", 
+            email: "moisescamposdaw@gmail.com", 
+            telefono: "(+34) 603 123 456",
+            ubicacion: "Canarias, España"
+        },
+        {
+            linkedIn: "<a href='https://www.linkedin.com/in/moisescap/'><img src='img/linkedin.png' width='26px'></a>",
+            github: "<a href='https://github.com/MoisesCDAW'><img src='img/github.png' width='26px'></a>",
+            instagram: "<a href='https://www.instagram.com/moisescamposp/'><img src='img/instagram.png' width='26px'></a>"
         }
     ],
     estudios: [
@@ -52,10 +57,27 @@ const misDatos = {
     proyectos: [
         {
             codigo: 1,
-            url: "...",
-            titulo: "...",
-            img: "...",
-            descripcion: "..."
+            url: "mixapp.moisescap.com",
+            titulo: "MIXAPP",
+            img: "img/portada_mixapp.png",
+            descripcion: `Es un programa con utilidades variadas. Contiene un tipo test y un juego de memorias sobre Los Simpsons.
+            También posee un formulario con la finalidad de llevar un control de la distribución de un producto`
+        },
+        {
+            codigo: 2,
+            url: "calendario.moisescap.com",
+            titulo: "Calendario",
+            img: "img/portada_calendario.png",
+            descripcion: `Calendario que muestra los días festivos del 2024 de Canarias. Permite filtrar por islas y marcar como favoritos a los festivos`
+        },
+        {
+            codigo: 3,
+            url: "trivia.moisescap.com",
+            titulo: "Trivia",
+            img: "img/portada_trivia.png",
+            descripcion: `Es un trivia con 10 preguntas de todo tipo de temas. Posee un temporizador para medir el tiempo de respuesta y 
+            un sistema de puntaje que mantiene el puntaje máximo de un usuario`
         }
     ]
-};    
+}; 
+
