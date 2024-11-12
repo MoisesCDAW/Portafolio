c3b140ba73320cbd8259a8e339262acd3c7ecb63
MoisesCDAW <moisescamposdaw@gmail.com>
2024-11-12 13:02:11 +0000

    Agregados datos personales

--- js/script.js
+++ js/script.js
@@ -1,7 +1,54 @@
 const misDatos = {
-    datosPerso: ["Moisés Alejandro", "Campos Perdomo", "moisescamposdaw@gmail.com", "603-123456"],
-    estudios: ["Bachillerato", "FP Desarrollo de Aplicaciones Web"],
-    experiencia: [],
+    datosPerso: [
+        "Moisés Alejandro Campos Perdomo", 
+        "07. 10. 2003", 
+        "Desarrollador de Aplicaciones Web",
+        "moisescamposdaw@gmail.com", 
+        "(+34) 603 123 456", 
+        "Canarias, España",
+        {
+            github: "github.com/MoisesCDAW",
+            linkedIn: "linkedin.com/in/moisescap/",
+            instagram: "instagram.com/moisescamposp/"
+        }
+    ],
+    estudios: [
+        {
+            estudio: "Bachillerato",
+            inicio: "Septiembre de 2015",
+            fin: "Julio de 2020",
+            sede: "Colegio Decroly",
+            ubicacion: "Venezuela"
+        },
+        {
+            estudio: "FP en Desarrollo de Aplicaciones Web",
+            inicio: "Septiembre de 2023",
+            fin: "Actualidad",
+            sede: "IES José María Pérez Pulido",
+            ubicacion: "La Palma, España"
+        }
+    ],
+    experiencia: [
+        {
+            puesto: "Soporte Técnico",
+            inicio: "Enero de 2022",
+            fin: "Julio de 2022",
+            sede: "Innovate Web",
+            ubicacion: "España",
+            descripcion: `Me encargué de proporcionar soporte técnico para las páginas web de los clientes 
+            y de resolver los inconvenientes técnicos internos de la empresa cuando surgían`
+        },
+        {
+            puesto: "Desarrollador Full Stack",
+            inicio: "Julio de 2022",
+            fin: "Actualidad",
+            sede: "Innovate Web",
+            ubicacion: "España",
+            descripcion: `Me encargo del desarrollo y mantenimiento de aplicaciones web, 
+            trabajando tanto en el frontend como en el backend. Además, me ocupé de la integración de bases de datos 
+            y de resolver problemas técnicos para garantizar el buen funcionamiento de las plataformas.`
+        }
+    ],
     proyectos: [
         {
             codigo: 1,
@@ -11,4 +58,4 @@ const misDatos = {
             descripcion: "..."
         }
     ]
-};
+};    
